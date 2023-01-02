9037e9971686434b5cc09c0d2ecf7a0757b6448b
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2023-01-01 16:45:20 -0800

    Command for debugging lexer tokens

---
+++ cmd/edible/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/HereIsKevin/edible/internal/logger"
+	"github.com/HereIsKevin/edible/internal/scanner"
+)
+
+func main() {
+	contents, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	source := string(contents)
+	logger := logger.New(source)
+
+	scannerStart := time.Now()
+	tokens := scanner.New(source, logger).Scan()
+	scannerEnd := float64(time.Since(scannerStart)) / float64(time.Millisecond)
+
+	fmt.Println("========== SCANNER:", scannerEnd, "ms ==========")
+
+	if logger.Log() {
+		os.Exit(1)
+	}
+
+	indent := 0
+
+	for _, token := range tokens {
+		switch token.Kind {
+		case scanner.TokenOpenParen,
+			scanner.TokenOpenBrack,
+			scanner.TokenOpenBrace,
+			scanner.TokenOpenBlock:
+			indent += 1
+			fmt.Print(token, "\n", strings.Repeat("    ", indent))
+		case scanner.TokenCloseParen,
+			scanner.TokenCloseBrack,
+			scanner.TokenCloseBrace,
+			scanner.TokenCloseBlock:
+			indent -= 1
+			fmt.Print("\n", strings.Repeat("    ", indent), token, " ")
+		case scanner.TokenEOF, scanner.TokenComma, scanner.TokenNewline:
+			fmt.Print(token, "\n", strings.Repeat("    ", indent))
+		default:
+			fmt.Print(token, " ")
+		}
+	}
+}
