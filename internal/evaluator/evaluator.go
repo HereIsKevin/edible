00a4858762c2b8e251a71daa8b631e5581e21bb4
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2023-01-07 00:56:14 -0800

    Partially working evaluator

--- internal/evaluator/evaluator.go
+++ internal/evaluator/evaluator.go
@@ -1,80 +1,230 @@
 package evaluator
 
 import (
-	"fmt"
-
 	"github.com/HereIsKevin/edible/internal/logger"
 	"github.com/HereIsKevin/edible/internal/parser"
 )
 
-type Evaluator struct {
-	expr   parser.Expr
-	logger *logger.Logger
-	value  any
+type refData struct {
+	parent    parser.Expr
+	value     parser.Expr
+	evaluated bool
+}
 
-	root   *Evaluator
-	parent *Evaluator
+type Evaluator struct {
+	expr     parser.Expr
+	logger   *logger.Logger
+	refDatas map[*parser.ExprRef]*refData
 }
 
 func New(expr parser.Expr, logger *logger.Logger) *Evaluator {
-	evaluator := &Evaluator{
-		expr:   expr,
-		logger: logger,
-		value:  nil,
+	return &Evaluator{
+		expr:     expr,
+		logger:   logger,
+		refDatas: map[*parser.ExprRef]*refData{},
 	}
-
-	// Root of root is itself.
-	evaluator.root = evaluator
-
-	// Parent of root is parent if it is a table since parent always refers to the
-	// closest table up the hierarchy, which is itself for a root table.
-	_, ok := evaluator.expr.(*parser.ExprTable)
-	if ok {
-		evaluator.parent = evaluator
-	}
-
-	return evaluator
 }
 
-func (evaluator *Evaluator) Evaluate() {
-	evaluator.evaluateStatic()
+func (evaluator *Evaluator) Evaluate() parser.Expr {
+	if _, ok := evaluator.expr.(*parser.ExprTable); ok {
+		evaluator.resolve(evaluator.expr, evaluator.expr)
+	} else {
+		evaluator.resolve(nil, evaluator.expr)
+	}
+
+	if err := evaluator.evaluate(evaluator.expr); err != nil {
+		evaluator.logger.AddError(err)
+	}
+
+	return evaluator.expr
 }
 
-func (evaluator *Evaluator) evaluateStatic() {
-	switch expr := evaluator.expr.(type) {
-	// Literal expressions
-	case *parser.ExprStr:
-		evaluator.value = expr.Value
-	case *parser.ExprBool:
-		evaluator.value = expr.Value
-	case *parser.ExprInt:
-		evaluator.value = expr.Value
-	case *parser.ExprFloat:
-		evaluator.value = expr.Value
+func (evaluator *Evaluator) resolve(parent parser.Expr, expr parser.Expr) {
+	switch expr := expr.(type) {
+	case *parser.ExprStr, *parser.ExprBool, *parser.ExprInt, *parser.ExprFloat:
+		// Skip literals
 
-	// Dynamic expressions
 	case *parser.ExprRef:
-		// References are always dynamic.
+		evaluator.refDatas[expr] = &refData{
+			parent:    parent,
+			value:     nil,
+			evaluated: false,
+		}
+
+		for _, key := range expr.Keys {
+			evaluator.resolve(parent, key)
+		}
+
 	case *parser.ExprUnary:
-		// Unary operations are often dynamic and are not containers, so it is not worth
-		// it to evaluate the possibly static ones.
+		evaluator.resolve(parent, expr.Right)
+
 	case *parser.ExprBinary:
-		// Similarly, binary operations are often dynamic, so it is not worth it to
-		// evaluate them.
+		evaluator.resolve(parent, expr.Left)
+		evaluator.resolve(parent, expr.Right)
 
-	// Container expressions
 	case *parser.ExprArray:
-		evaluator.evaluateStaticArray(expr)
+		for _, item := range expr.Items {
+			evaluator.resolve(parent, item)
+		}
+
 	case *parser.ExprTable:
-		evaluator.evaluateStaticTable(expr)
+		for _, item := range expr.Items {
+			evaluator.resolve(expr, item.Key)
+			evaluator.resolve(expr, item.Value)
+
+			if item.Parent != nil {
+				evaluator.resolve(expr, item.Parent)
+			}
+		}
 	}
 }
 
-func (evaluator *Evaluator) evaluateStaticArray(array *parser.ExprArray) {
-	evaluators := []*Evaluator{}
+func (evaluator *Evaluator) evaluate(expr parser.Expr) error {
+	switch expr := expr.(type) {
+	case *parser.ExprStr, *parser.ExprBool, *parser.ExprInt, *parser.ExprFloat:
+		// Skip literals
 
-	for index, item := range array.Items {
-		switch item.(type) {
+	case *parser.ExprRef:
+		if err := evaluator.evaluateRef(expr); err != nil {
+			return err
+		}
+
+	case *parser.ExprUnary:
+		panic("evaluateUnary is not implemented")
+
+	case *parser.ExprBinary:
+		panic("evaluateBinary is not implemented")
+
+	case *parser.ExprArray:
+		panic("evaluateArray is not implemented")
+
+	case *parser.ExprTable:
+		if err := evaluator.evaluateTable(expr); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (evaluator *Evaluator) evaluateRef(ref *parser.ExprRef) error {
+	data := evaluator.refDatas[ref]
+
+	// Do not evaluate already evaluated references.
+	if data.evaluated {
+		return nil
+	}
+
+	// Set status before starting to prevent infinite recursion.
+	data.evaluated = true
+
+	// Choose base expression based on modifier.
+	expr := evaluator.expr
+	if ref.Modifier == parser.RefRelative {
+		expr = data.parent
+	}
+
+loop:
+	for _, rawKey := range ref.Keys {
+		// Unwrap all references in the current key.
+		keyExpr, err := evaluator.unwrap(rawKey)
+		if err != nil {
+			return err
+		}
+
+		// Unwrap all references in the current value.
+		currentExpr, err := evaluator.unwrap(expr)
+		if err != nil {
+			return err
+		}
+
+		switch current := currentExpr.(type) {
+		case *parser.ExprArray:
+			panic("ExprArray is not implemented for evaluateRef")
+
+		case *parser.ExprTable:
+			// Make sure the key is a string.
+			key, ok := keyExpr.(*parser.ExprStr)
+			if !ok {
+				return &logger.Error{
+					Message: "Expect string for table key.",
+					Span:    rawKey.Span(),
+				}
+			}
+
+			// TODO: Use map for better performance.
+			for _, item := range current.Items {
+				// Attempt to evaluate key from table, skip on failure.
+				itemKeyExpr, err := evaluator.unwrap(item.Key)
+				if err != nil {
+					continue
+				}
+
+				// Make sure the item key is a string.
+				itemKey, ok := itemKeyExpr.(*parser.ExprStr)
+				if !ok {
+					continue
+				}
+
+				// Go on to the next iteration if the keys match.
+				if key.Value == itemKey.Value {
+					expr = item.Value
+					continue loop
+				}
+			}
+
+			// Completed loop means none of the keys match.
+			return &logger.Error{
+				Message: "Key not found.",
+				Span:    rawKey.Span(),
+			}
+
+		default:
+			return &logger.Error{
+				Message: "Expect array or table.",
+				Span:    expr.Span(),
+			}
+		}
+	}
+
+	data.value = expr
+
+	return nil
+}
+
+func (evaluator *Evaluator) evaluateTable(table *parser.ExprTable) error {
+	for _, item := range table.Items {
+		if err := evaluator.evaluate(item.Key); err != nil {
+			return err
+		}
+
+		key, err := evaluator.unwrap(item.Key)
+		if err != nil {
+			return err
+		}
+
+		item.Key = key
+
+		// TODO: Evaluate parents.
+
+		if err := evaluator.evaluate(item.Value); err != nil {
+			return err
+		}
+
+		value, err := evaluator.unwrap(item.Value)
+		if err != nil {
+			return err
+		}
+
+		item.Value = value
+	}
+
+	return nil
+}
+
+func (evaluator *Evaluator) unwrap(expr parser.Expr) (parser.Expr, error) {
+	for {
+		switch current := expr.(type) {
 		case *parser.ExprStr,
 			*parser.ExprBool,
 			*parser.ExprInt,
@@ -82,100 +232,30 @@ func (evaluator *Evaluator) evaluateStaticArray(array *parser.ExprArray) {
 			*parser.ExprArray,
 			*parser.ExprTable:
 
-			// Evaluate literals and containers as static items.
-			itemEvaluator := New(item, evaluator.logger)
-			itemEvaluator.root = evaluator.root
-			itemEvaluator.parent = evaluator.parent
-			itemEvaluator.evaluateStatic()
+			// Exit if the expression is a concrete value.
+			return expr, nil
 
-			// Add the evaluator.
-			evaluators = append(evaluators, itemEvaluator)
+		case *parser.ExprRef:
+			// Evaluate the reference.
+			evaluator.evaluate(current)
 
-			// Mark the item as completed.
-			array.Items[index] = nil
+			// Extract the data within reference.
+			data := evaluator.refDatas[current]
+			if data.value == nil {
+				return nil, &logger.Error{
+					Message: "Failed to unwrap reference.",
+					Span:    expr.Span(),
+				}
+			}
 
-		case *parser.ExprRef, *parser.ExprUnary, *parser.ExprBinary:
-			// Skip over dynamic items.
-			evaluators = append(evaluators, nil)
+			// Repeat with expression from reference.
+			expr = data.value
+
+		case *parser.ExprUnary:
+			panic("ExprUnary is not implemented for evaluateKey")
+
+		case *parser.ExprBinary:
+			panic("ExprBinary is not implemented for evaluateKey")
 		}
-	}
-
-	evaluator.value = evaluators
-}
-
-func (evaluator *Evaluator) evaluateStaticTable(table *parser.ExprTable) {
-	evaluators := map[string]*Evaluator{}
-
-	for index, item := range table.Items {
-		// Only evaluate accept static string keys.
-		key, ok := item.Key.(*parser.ExprStr)
-		if !ok {
-			continue
-		}
-
-		// Only evaluate items without parents.
-		if item.Inherits != nil {
-			continue
-		}
-
-		switch item.Value.(type) {
-		case *parser.ExprStr,
-			*parser.ExprBool,
-			*parser.ExprInt,
-			*parser.ExprFloat,
-			*parser.ExprArray,
-			*parser.ExprTable:
-
-			// Evaluate literals and containers as static items.
-			valueEvaluator := New(item.Value, evaluator.logger)
-			valueEvaluator.root = evaluator.root
-			valueEvaluator.parent = evaluator.parent
-			valueEvaluator.evaluateStatic()
-
-			// Add the evaluator.
-			evaluators[key.Value] = valueEvaluator
-
-			// Mark the item as completed.
-			table.Items[index] = nil
-
-		case *parser.ExprRef, *parser.ExprUnary, *parser.ExprBinary:
-			// Skip over dynamic items.
-		}
-	}
-
-	evaluator.value = evaluators
-}
-
-func (evaluator *Evaluator) String() string {
-	switch value := evaluator.value.(type) {
-	case string:
-		return fmt.Sprintf("\"%s\"", value)
-	case bool:
-		return fmt.Sprintf("%t", value)
-	case int64:
-		return fmt.Sprintf("%d", value)
-	case float64:
-		return fmt.Sprintf("%f", value)
-
-	case []*Evaluator:
-		debugSlice := []string{}
-
-		for _, item := range value {
-			debugSlice = append(debugSlice, item.String())
-		}
-
-		return logger.DebugSlice(debugSlice)
-
-	case map[string]*Evaluator:
-		debugMap := map[string]string{}
-
-		for key, value := range value {
-			debugMap[key] = value.String()
-		}
-
-		return logger.DebugMap(debugMap)
-
-	default:
-		return "Unknown"
 	}
 }
